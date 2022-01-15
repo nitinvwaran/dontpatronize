03539f67a5e7b8f4b281745b62e49a97c8d22530
nitin <nitin.vwaran@gmail.com>
2022-01-15 11:24:35 +0800

    inference

--- inference.py
+++ inference.py
@@ -3,11 +3,11 @@ import torch
 import argparse
 
 from preprocessingutils import PreprocessingUtils
-from modules import LSTMAttention
+from modules import LSTMAttention, BertModels,CNNBert
 from tqdm import tqdm
 
 class Inference():
-    def __init__(self,pclfile,categoriesfile,testfile,modeltype='rnn',bertmodeltype='rawdistilbert',maxlen=256,devbatchsize=1000):
+    def __init__(self,bestmodelpath, pclfile,categoriesfile,testfile,modeltype='rnn',bertmodeltype='rawdistilbert',devbatchsize=1000,rnntype='lstm',maxlenphrase=96,maxlentext=512):
 
         self.pclfile = pclfile
         self.categoriesfile = categoriesfile
@@ -18,15 +18,17 @@ class Inference():
 
         self.modeltype = modeltype
         self.bertmodeltype = bertmodeltype
+        self.rnntype = rnntype
         self.devbatchsize = devbatchsize
 
-        #if modeltype == 'bert':
-        #    self.model = BertModels(bertmodeltype=bertmodeltype, maxlen=maxlen)
-        #elif modeltype == 'rnn':
-        #    self.model = LSTMAttention(lstmtype='lstm', bertmodeltype=bertmodeltype)
+        if modeltype == 'bert':
+            self.model = BertModels(bertmodeltype=bertmodeltype, maxlen=maxlentext)
+        elif modeltype == 'rnn':
+            self.model = LSTMAttention(rnntype=rnntype,bertmodeltype=bertmodeltype,maxlentext=maxlentext,maxlenphrase=maxlenphrase)
+        else:
+            self.model = CNNBert(maxlen=maxlentext,bertmodeltype=bertmodeltype)
 
-        self.model = LSTMAttention(bertmodeltype=bertmodeltype,maxlen=maxlen)
-        checkpoint = torch.load('/home/nitin/Desktop/dontpatronize/dontpatronize/data/checkpoint/model_test20.3863080684596577.pt')
+        checkpoint = torch.load(bestmodelpath)
         self.model.load_state_dict(checkpoint)
 
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -62,25 +64,33 @@ class Inference():
             preds = preds.loc[preds.groupby(['lineid'])['preds'].idxmax()].reset_index(drop=True)
             preds.set_index('lineid')
 
-            preds.to_csv('inference.csv')
+            preds.to_csv('inference_' + self.modeltype + '_' + self.bertmodeltype + '_' + self.rnntype + '.tsv',sep='\t')
 
 
 def main():
+
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--lr', type=float, default=1e-5)
-    parser.add_argument('--maxlen', type=int, default=224)
-    parser.add_argument('--devbat', type=int, default=500)
-    parser.add_argument('--wd', type=float, default=0.01)
-    parser.add_argument('--bertmodeltype', type=str, default='rawdistilbert')
-    parser.add_argument('--modeltype', type=str, default='rnn')
+
+    parser.add_argument('--maxlentext', type=int, default=224)
+    parser.add_argument('--maxlenphrase', type=int, default=96)
 
     args = parser.parse_args()
+
+    with open('bestmodel.txt','r') as i:
+        for line in i.readlines():
+            bestmodelpath = str(line).strip().split()[0]
+
+    params = bestmodelpath.split('_')
+    modeltype = params[1].strip()
+    bertmodeltype = params[2].strip()
+    rnntype = params[3].strip()
+
 
     pclfile = 'data/dontpatronizeme_v1.4/dontpatronizeme_pcl.tsv'
     categoriesfile = 'data/dontpatronizeme_v1.4/dontpatronizeme_categories.tsv'
 
-    inf = Inference(pclfile,categoriesfile,None,maxlen=224,devbatchsize=500)
+    inf = Inference(bestmodelpath=bestmodelpath,pclfile=pclfile,categoriesfile=categoriesfile,testfile=None,maxlentext=args.maxlentext,maxlenphrase=args.maxlenphrase,devbatchsize=500,modeltype=modeltype,bertmodeltype=bertmodeltype,rnntype=rnntype)
     inf.inference()
 
 
